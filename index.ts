fb36091707906e6eeb6f549157ab3c7b88aed061
Gerry308 <gwtacticalau@gmail.com>
2026-02-01 05:29:21 +1000

    Update index.ts

--- index.ts
+++ index.ts
@@ -61,7 +61,10 @@ app.use((req, res, next) => {
 });
 
 (async () => {
-// TODO: Add route handlers back
+  await registerRoutes(httpServer, app);
+  
+  app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
+
 
   app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
     const status = err.status || err.statusCode || 500;
